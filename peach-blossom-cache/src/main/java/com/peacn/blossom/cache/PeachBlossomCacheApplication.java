aed7c31199769307f1f6cec65a7e959aa591af48
taoruanliang <929046425@qq.com>
2022-03-05 15:06:05 +0800

    init cache

---
+++ peach-blossom-cache/src/main/java/com/peacn/blossom/cache/PeachBlossomCacheApplication.java
@@ -0,0 +1,13 @@
+package com.peacn.blossom.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachBlossomCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeachBlossomCacheApplication.class, args);
+    }
+
+}
---
+++ peach-blossom-cache/src/test/java/com/peacn/blossom/cache/PeachBlossomCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peacn.blossom.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeachBlossomCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
